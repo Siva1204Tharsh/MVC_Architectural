cb17f1086c8887bd85755feba1801563fe0463fe
Siva1204Tharsh <sivasivapatham12@gmail.com>
2024-08-17 10:10:39 +0530

    First Commit

---
+++ src/practical_mvc_calculator/Practical_MVC_Calculator.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package practical_mvc_calculator;
+
+/**
+ *
+ * @author SIVA1201
+ */
+public class Practical_MVC_Calculator {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
